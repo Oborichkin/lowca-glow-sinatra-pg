07d70227832ef657ea0221d6e6bbb78434492078
Pavel Oborin <pavel@oborin.xyz>
2020-11-15 23:34:21 +0300

    Added migrations

--- api_sinatra_postgres.rb
+++ api_sinatra_postgres.rb
@@ -7,6 +7,16 @@ require 'pg'
 require 'json'
 require 'multi_json'
 
+DB = Sequel.connect(
+  adapter: :postgres,
+  database: 'sinatra_seq_dev',
+  host: 'localhost',
+  password: 'password',
+  user: 'sinatra_admin',
+  max_connections: 10,
+  # logger: Logger.new('log/db.log')
+)
+
 %w{controllers models routes}.each {|dir| Dir.glob("./#{dir}/*.rb", &method(:require))}
 
 get '/' do
