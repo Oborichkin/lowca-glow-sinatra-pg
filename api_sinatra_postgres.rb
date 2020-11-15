cecb62c821cdc8209d7d456ee19a513baaab4b44
Pavel Oborin <pavel@oborin.xyz>
2020-11-15 23:57:32 +0300

    Added models

--- api_sinatra_postgres.rb
+++ api_sinatra_postgres.rb
@@ -1,24 +1,32 @@
-require 'sinatra'
-require 'sinatra/base'
-require 'sinatra/namespace'
-require 'sequel'
-require 'sequel/extensions/seed'
-require 'pg'
-require 'json'
-require 'multi_json'
+require "sinatra"
+require "sinatra/base"
+require "sinatra/namespace"
+require "sequel"
+require "sequel/extensions/seed"
+require "pg"
+require "json"
+require "multi_json"
 
 DB = Sequel.connect(
   adapter: :postgres,
-  database: 'sinatra_seq_dev',
-  host: 'localhost',
-  password: 'password',
-  user: 'sinatra_admin',
+  database: "sinatra_seq_dev",
+  host: "localhost",
+  password: "password",
+  user: "sinatra_admin",
   max_connections: 10,
   # logger: Logger.new('log/db.log')
 )
 
-%w{controllers models routes}.each {|dir| Dir.glob("./#{dir}/*.rb", &method(:require))}
+%w{controllers models routes}.each { |dir| Dir.glob("./#{dir}/*.rb", &method(:require)) }
 
-get '/' do
-    "Hi! I am api_sinatra_postgres!\n\n"
+before do
+  content_type "application/json" # Uncomment - To see perfectly in POSTMAN - in tab Pretty
+end
+
+def collection_to_api(collection)
+  MultiJson.dump(collection.map { |s| s.to_api })
+end
+
+get "/" do
+  "Hi! I am api_sinatra_postgres!\n\n"
 end
---
+++ models/company.rb
@@ -0,0 +1,2 @@
+class Company < Sequel::Model
+end
